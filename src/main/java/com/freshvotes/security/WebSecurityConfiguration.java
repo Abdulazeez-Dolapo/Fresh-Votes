8d7a2c4eb496306ab05ce9e0b79e69d8ab323719
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-28 08:50:59 +0100

    set up login page

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -9,18 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("user")
-			.password("password")
-			.roles("USER");
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests()
-			.antMatchers("/").permitAll()
-			.anyRequest().hasRole("USER").and()
-			.formLogin().loginPage("/login").permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().anyRequest().hasRole("USER").and()
+				.formLogin().loginPage("/login").permitAll();
 	}
 }
